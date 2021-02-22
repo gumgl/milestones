602e89b340cec299a4c1a5f94129cc195d410920
Guillaume <gumgl@users.noreply.github.com>
2021-02-21 17:05:10 -0800

    /src: MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Generator, Generators} from './Generators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      refDate: new Date(1969, 6, 24),
+      powers10: true,
+      n10x: false,
+      repdigit: false,
+      powers2: false,
+      factorial: false,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : 
+                  target.type === 'date' ? new Date(target.value) :
+                  target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <form>
+        <label>
+          Date of reference: <br/>
+          <input
+            name="refDate"
+            type="date"
+            value={this.state.refDate.toISOString().split('T')[0]}
+            onChange={this.handleInputChange} />
+        </label>
+        <fieldset>
+        <legend>What do you care about?</legend>
+        <ul>
+          <li>
+            <label>
+              <input
+                name="powers10"
+                type="checkbox"
+                checked={this.state.powers10}
+                onChange={this.handleInputChange} />
+              10^x
+          </label>
+          </li>
+          <li>
+            <label>
+              <input
+                name="n10x"
+                type="checkbox"
+                checked={this.state.n10x}
+                onChange={this.handleInputChange} />
+              n * 10^x (for small n)
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                name="repdigit"
+                type="checkbox"
+                checked={this.state.repdigit}
+                onChange={this.handleInputChange} />
+              [n]^x (repeated digit)
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                name="powers2"
+                type="checkbox"
+                checked={this.state.powers2}
+                onChange={this.handleInputChange} />
+              2^x
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                name="factorial"
+                type="checkbox"
+                checked={this.state.factorial}
+                onChange={this.handleInputChange} />
+              x! (factorial)
+            </label>
+          </li>
+        </ul>
+        </fieldset>
+      </form>
+      <p>We are going on {this.state.refDate.toISOString().split('T')[0]}</p>
+
+      <MilestonesList state={this.state} />
+      
+      </div>
+    );
+  }
+}
+
+function MilestonesList(props) {
+  let list = computeMilestones(props.state).map((milestone) =>
+    <li key={milestone.date.getTime()+"-"+milestone.value}>
+    <span title={milestone.date}>{milestone.date.toISOString().split('T')[0]}</span>: 
+    <span title={milestone.value}>{milestone.label}</span> - 
+    <a target="_blank" rel="noreferrer" href={"https://www.wolframalpha.com/input/?i="+encodeURIComponent(props.state.refDate.toISOString().split('T')[0]+" + "+milestone.label)}>Verify</a></li>);
+  return <ul>{list}</ul>;
+}
+
+function computeMilestones(state) {
+  const units = [
+    {ms: 1000, name: "seconds"},
+    {ms: 1000*60, name: "minutes"},
+    {ms: 1000*60*60, name: "hours"},
+    {ms: 1000*60*60*24, name: "days"},
+    {ms: 1000*60*60*24*7, name: "weeks"},
+    // months TBD
+  ];
+
+  let milestones = [],
+      refTime = state.refDate.getTime(),
+      now = Date.now();
+
+  for (const unit of units)
+    for (const g of Generators)
+      if (state[g.name]) {
+        let future = Generator.filter((item) => refTime + item.value * unit.ms > now - 1000*60*60*24*31, g.gf);
+        let nearFuture = Generator.takeWhile(
+          (item) => refTime + item.value * unit.ms < now + 1000*60*60*24*365*10,
+          future()
+          );
+        let tagged = nearFuture.map((item) => ({
+          date: new Date(refTime + item.value * unit.ms),
+          label: g.format(item) + " " + unit.name,
+          value: item.value
+        }));
+        milestones = milestones.concat(tagged);
+      }
+
+  milestones.sort((a,b) => a.date - b.date);
+
+  return milestones;
 }
 
 export default App;
---
+++ src/Generators.js
@@ -0,0 +1,90 @@
+export const Generator =
+{
+  map: (f,gf) => function* (...args)
+    {
+      for (const x of gf (...args))
+        yield f (x)
+    },
+  filter: (f,gf) => function* (...args)
+    {
+      for (const x of gf (...args))
+        if (f (x))
+          yield x
+    },
+  take: function(n, gi) {
+    let list=[];
+    for (let i=0, c=gi.next();
+      !c.done && i<n;
+      i++, c=gi.next())
+      list[i] = c.value;
+    return list;
+  },
+  takeWhile: function(f, gi) {
+    let list = [];
+    for (let c=gi.next();
+      !c.done && f(c.value);
+      c=gi.next())
+      list.push(c.value);
+    return list;
+  }
+};
+
+export const Generators = [
+  {
+    name:"powers10",
+    gf: function*() {
+      for (let n = 1; ; n++)
+        yield {
+          params: [n],
+          value: Math.pow(10,n)
+        };
+    },
+    format: (item) => item.value.toLocaleString()//"10^" + item.params[0]
+  },
+  {
+    name:"powers2",
+    gf: function*() {
+      for (let n = 1; ; n++)
+        yield {
+          params: [n],
+          value: Math.pow(2,n)
+        };
+    },
+    format: (item) => "2^" + item.params[0]
+  },
+  {
+    name:"repdigit",
+    gf: function*() {
+      for (let b=1; ; b=b*10+1)
+        for (let n=1; n<=9; n++)
+          yield {
+            params: [b, n],
+            value: b*n
+          };
+    },
+    format: (item) => item.value.toLocaleString()
+  },
+  {
+    name:"n10x",
+    gf: function*() {
+      for (let x=1; ; x++)
+        for (let n=2; n<=9; n++)
+          yield {
+            params: [n, x],
+            value: n * Math.pow(10, x)
+          };
+    },
+    format: (item) => item.value.toLocaleString()
+  },
+  {
+    name:"factorial",
+    gf: function*() {
+      for (let n = 1, m = 1; ; n++, m *= n)
+        yield {
+          params: [n],
+          value: m
+        };
+    },
+    format: (item) => item.params[0] + "!"
+  },
+];
