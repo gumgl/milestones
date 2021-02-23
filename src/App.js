027cf4c2010a782d34945e1453e4e2830841bb4b
Guillaume <gumgl@users.noreply.github.com>
2021-02-22 18:52:52 -0800

    /src: Refactor from React class component to using hooks

--- src/App.js
+++ src/App.js
@@ -1,132 +1,134 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import {Generator, Generators} from './Generators';
 import { Button } from '@material-ui/core';
 
-class App extends React.Component {
+function App() {
+  const [refDate, setRefDate] = useState(new Date(1969, 6, 24));
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      refDate: new Date(1969, 6, 24),
-      powers10: true,
-      n10x: false,
-      repdigit: false,
-      powers2: false,
-      factorial: false,
-      lookandsay: false
-    };
+  const [genOptions, setGenOptions] = useState({
+    powers10: true,
+    n10x: false,
+    repdigit: false,
+    powers2: false,
+    factorial: false,
+    lookandsay: false
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
+  const handleGenInputChange = (event) => {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : 
-                  target.type === 'date' ? new Date(target.value) :
+    const value = target.type === 'checkbox' ? target.checked :
                   target.value;
     const name = target.name;
 
-    this.setState({
+    setGenOptions({
+      ...genOptions,
       [name]: value
     });
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <form>
-        <label>
-          Date of reference: <br/>
-          <input
-            name="refDate"
-            type="date"
-            value={this.state.refDate.toISOString().split('T')[0]}
-            onChange={this.handleInputChange} />
-        </label>
-        <fieldset>
-        <legend>What do you care about?</legend>
-        <ul>
-          <li>
-            <label>
-              <input
-                name="powers10"
-                type="checkbox"
-                checked={this.state.powers10}
-                onChange={this.handleInputChange} />
-              10^x
-          </label>
-          </li>
-          <li>
-            <label>
-              <input
-                name="n10x"
-                type="checkbox"
-                checked={this.state.n10x}
-                onChange={this.handleInputChange} />
-              n * 10^x (for small n)
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                name="repdigit"
-                type="checkbox"
-                checked={this.state.repdigit}
-                onChange={this.handleInputChange} />
-              [n]^x (repeated digit)
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                name="powers2"
-                type="checkbox"
-                checked={this.state.powers2}
-                onChange={this.handleInputChange} />
-              2^x
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                name="factorial"
-                type="checkbox"
-                checked={this.state.factorial}
-                onChange={this.handleInputChange} />
-              x! (factorial)
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                name="lookandsay"
-                type="checkbox"
-                checked={this.state.lookandsay}
-                onChange={this.handleInputChange} />
-              Look-and-say terms (<a href="https://oeis.org/A005150" target="_blank" rel="noreferrer">A005150</a>)
-            </label>
-          </li>
-        </ul>
-        </fieldset>
-      </form>
-      <p>Since {this.state.refDate.toDateString()}...</p>
+  };
 
-      <MilestonesList state={this.state} />
-      
-      </div>
-    );
-  }
+  const handleDateInputChange = (e) => {
+    const d = new Date(e.target.value);
+
+    if (!isNaN(d))
+      setRefDate(d);
+    else
+      console.error("Incorrect date input: ", e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <form>
+      <label>
+        Date of reference: <br/>
+        <input
+          name="refDate"
+          type="date"
+          value={refDate.toISOString().split('T')[0]}
+          onChange={handleDateInputChange} />
+      </label>
+      <fieldset>
+      <legend>What do you care about?</legend>
+      <ul>
+        <li>
+          <label>
+            <input
+              name="powers10"
+              type="checkbox"
+              checked={genOptions.powers10}
+              onChange={handleGenInputChange} />
+            10^x
+        </label>
+        </li>
+        <li>
+          <label>
+            <input
+              name="n10x"
+              type="checkbox"
+              checked={genOptions.n10x}
+              onChange={handleGenInputChange} />
+            n * 10^x (for small n)
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              name="repdigit"
+              type="checkbox"
+              checked={genOptions.repdigit}
+              onChange={handleGenInputChange} />
+            [n]^x (repeated digit)
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              name="powers2"
+              type="checkbox"
+              checked={genOptions.powers2}
+              onChange={handleGenInputChange} />
+            2^x
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              name="factorial"
+              type="checkbox"
+              checked={genOptions.factorial}
+              onChange={handleGenInputChange} />
+            x! (factorial)
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              name="lookandsay"
+              type="checkbox"
+              checked={genOptions.lookandsay}
+              onChange={handleGenInputChange} />
+            Look-and-say terms (<a href="https://oeis.org/A005150" target="_blank" rel="noreferrer">A005150</a>)
+          </label>
+        </li>
+      </ul>
+      </fieldset>
+    </form>
+    <p>Since {refDate.toDateString()}...</p>
+
+    <MilestonesList refDate={refDate} genOptions={genOptions} />
+    
+    </div>
+  );
 }
 
 function MilestonesList(props) {
-  let list = computeMilestones(props.state).map((milestone) =>
+  let list = computeMilestones(props.refDate, props.genOptions).map((milestone) =>
     <li key={milestone.date.getTime()+"-"+milestone.value}>
-    <span title={milestone.date}>{milestone.date.toISOString().split('T')[0]}</span>: <span title={milestone.value}>{milestone.label}</span> - <a target="_blank" rel="noreferrer" href={"https://www.wolframalpha.com/input/?i="+encodeURIComponent(props.state.refDate.toISOString().split('T')[0]+" + "+milestone.label)}>Verify</a></li>);
+    <span title={milestone.date}>{milestone.date.toISOString().split('T')[0]}</span>: <span title={milestone.value}>{milestone.label}</span> - <a target="_blank" rel="noreferrer" href={"https://www.wolframalpha.com/input/?i="+encodeURIComponent(props.refDate.toISOString().split('T')[0]+" + "+milestone.label)}>Verify</a></li>);
   return <ul>{list}</ul>;
 }
 
-function computeMilestones(state) {
+function computeMilestones(refDate, genOptions) {
   const units = [
     {ms: 1000, name: "seconds"},
     {ms: 1000*60, name: "minutes"},
@@ -137,12 +139,12 @@ function computeMilestones(state) {
   ];
 
   let milestones = [],
-      refTime = state.refDate.getTime(),
+      refTime = refDate.getTime(),
       now = Date.now();
 
   for (const unit of units)
     for (const g of Generators)
-      if (state[g.name]) {
+      if (genOptions[g.name]) {
         let future = Generator.filter((item) => refTime + item.value * unit.ms > now - 1000*60*60*24*365, g.gf);
         let nearFuture = Generator.takeWhile(
           (item) => refTime + item.value * unit.ms < now + 1000*60*60*24*365*10,
