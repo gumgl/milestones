6c68d89b9194c72b5c17b71548d1cb23a3f9524f
Guillaume <gumgl@users.noreply.github.com>
2021-04-04 00:31:15 -0700

    Custom sequence options display order and subsection

--- src/SequenceSelector.js
+++ src/SequenceSelector.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Sequences } from './Sequences';
 
 import Typography from '@material-ui/core/Typography';
@@ -6,8 +6,12 @@ import Box from '@material-ui/core/Box';
 import Checkbox from '@material-ui/core/Checkbox';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
-
 import InfoIcon from '@material-ui/icons/Info';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -17,19 +21,55 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 export function SequenceSelector(props) {
 
-  const list = Sequences.map(s =>
-    <SequenceOption
-      key={s.id}
-      sequence={s}
-      sequenceOptions={props.sequenceOptions}
-      setSequenceOption={props.setSequenceOption}
-    />);
+  const mainDisplayList = [
+    "powers10",
+    "1ton",
+    "nto1",
+    "repdigit",
+    "n10x",
+  ];
+  const bonusDisplayList = [
+    "powers2",
+    "factorial",
+    "lookandsay",
+    "fib",
+  ];
+
+  const [proMode, setProMode] = useState(false);
 
   return (
     <Box>
-      <Typography>What do you care about?</Typography>
-      <List>{list}</List>
+      <Typography>Pick number families:</Typography>
+      <SequenceOptionsList displayOptions={mainDisplayList} moduleProps={props} />
+
+      <Accordion
+        expanded={proMode}
+        onChange={e => setProMode(!proMode)}
+        variant="outlined">
+
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Nerd mode</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <SequenceOptionsList displayOptions={bonusDisplayList} moduleProps={props} />
+        </AccordionDetails>
+      </Accordion>
     </Box>);
+}
+
+function SequenceOptionsList(props) {
+  return <List>
+    {props.displayOptions.map(id => {
+      console.log(id);
+      const sequence = Sequences.find(s => s.id === id);
+
+      return <SequenceOption
+        key={sequence.id}
+        sequence={sequence}
+        moduleProps={props.moduleProps}
+      />;
+    })}
+  </List>;
 }
 
 function SequenceOption(props) {
@@ -37,12 +77,12 @@ function SequenceOption(props) {
 
   return (
     <ListItem key={s.id}
-      onClick={() => props.setSequenceOption(s.id, !props.sequenceOptions.get(s.id))}
+      onClick={() => props.moduleProps.setSequenceOption(s.id, !props.moduleProps.sequenceOptions.get(s.id))}
       dense button>
 
       <ListItemIcon>
         <Checkbox
-          checked={props.sequenceOptions.get(s.id)}
+          checked={props.moduleProps.sequenceOptions.get(s.id)}
           name={s.id}
           edge="start"
           tabIndex={-1}
